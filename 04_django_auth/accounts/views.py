051a7904cd27171606024fe8ddb65dd3b5d43a98
unhhyyeexx <unhhyyee66@gmail.com>
2022-04-12 13:33:50 +0900

    decorator

--- 04_django_auth/accounts/views.py
+++ 04_django_auth/accounts/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.http import require_http_methods, require_POST
 
 # Create your views here.
-
+@require_http_methods(['GET', 'POST'])
 def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
@@ -18,6 +19,7 @@ def login(request):
     }
     return render(request, 'accounts/login.html', context)
 
+@require_POST
 def logout(request):
     auth_logout(request)
     return redirect('articles:index')
