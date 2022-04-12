c8d9165b3e0e595e5706aa096753f04532461204
unhhyyeexx <unhhyyee66@gmail.com>
2022-04-12 14:33:22 +0900

    auto_login

--- 04_django_auth/accounts/views.py
+++ 04_django_auth/accounts/views.py
@@ -33,7 +33,8 @@ def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            auth_login(request, user)
             return redirect('articles:index')
     else:
         form = UserCreationForm()
